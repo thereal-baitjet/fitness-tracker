c7de82123d56a910903f1cdf2c7f658d46fd954b
Juan Santos <62819163+thereal-baitjet@users.noreply.github.com>
2021-06-05 09:29:06 -0400

    setup server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+//set port
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+
+//parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParser: true});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}..`);
+})
