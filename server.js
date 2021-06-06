7fa8ceade4750484be582a67c9f3636f1ea4040f
Juan Santos <62819163+thereal-baitjet@users.noreply.github.com>
2021-06-06 13:13:08 -0400

    api routes and modules

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 //set port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -12,6 +12,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParser: true});
+
+app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/api-routes.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}..`);
